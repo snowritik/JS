25606d117a7ad3c796c45cd3e9e18990a9aeec92
snowritik <112396713+snowritik@users.noreply.github.com>
2024-11-18 12:58:16 +0000

    changes commited

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,23 @@
-console.log('Ritik')
+// console.log('Ritik')
+
+let score  = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+let isLoggedIn = "Ritik"
+
+let bolleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(bolleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
